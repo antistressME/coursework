a1d0fd7e18f0e675f6e4708491f3312435f53e29
Anastasia <antipinaanastasia0112@gmail.com>
2025-09-24 15:04:37 +0300

    add decotator write_data_to_csv

--- src/reports.py
+++ src/reports.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import pathlib as Path
 
@@ -7,6 +8,24 @@ from read_xlsx import get_operations_from_xlsx
 from src.main import BASEDIR
 
 
+def write_data_to_csv(func):
+    """Декоратор, записывающий данные в файл."""
+
+    def inner(*args, **kwargs):
+        result = func(*args, **kwargs)
+        result_dict = result.to_dict("index")
+        fieldnames = list(result.columns)
+        path_to_file = Path.Path(BASEDIR / "data" / "reports.csv")
+        with open(path_to_file, "w") as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for value in result_dict.values():
+                writer.writerow(value)
+
+    return inner
+
+
+@write_data_to_csv
 def get_expenses_by_category(data: pd.DataFrame, category: str, date=datetime.datetime.now()) -> pd.DataFrame:
     """Возвращает траты по заданной категории за последние три месяца (90 дней)."""
 
@@ -24,8 +43,3 @@ if __name__ == "__main__":
     path_to_file = Path.Path(BASEDIR / "data" / "operations.xlsx")
     transactions = get_operations_from_xlsx(path_to_file)
     result = get_expenses_by_category(transactions, "Наличные", "20.02.2018")
-    xlsx_dict = result.to_dict("index")
-    xlsx_list = []
-    for value in xlsx_dict.values():
-        xlsx_list.append(dict(value))
-    print(xlsx_list)
